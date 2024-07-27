1d1e71fd94149453a6878d77a660cb21971ab8ce
hengtseChou <hengtse.me@gmail.com>
2024-07-27 17:45:05 +0800

    window rule

--- hypr/scripts/line.sh
+++ hypr/scripts/line.sh
@@ -4,5 +4,5 @@ chromium \
  --ozone-platform=wayland \
  --enable-wayland-ime \
  --app=chrome-extension://ophjlpahpchlmihnnnihgmmeilfjmjjc/index.html 
-# --window-size=800,600 \
+# --window-size=800,1500 \
 # --window-position=400,100
