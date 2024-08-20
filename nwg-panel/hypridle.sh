653a8a90544e7fba425d3adbd27a8cc3a526a3ea
hengtseChou <hankthedev@gmail.com>
2024-08-20 17:21:05 +0800

    cleanup unneeded config

--- nwg-panel/hypridle.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-hypridle=$(pgrep -x hypridle)
-if [ -z $hypridle ]; then
-    echo "$HOME/Dotfiles/nwg-panel/icons/unlock.svg"
-else
-    echo "$HOME/Dotfiles/nwg-panel/icons/lock.svg"
-fi
--- nwg-panel/power-profile-daemon.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-current=$(powerprofilesctl get)
-if [[ $current == "performance" ]]; then
-    echo "$HOME/Dotfiles/nwg-panel/icons/power-profile-performance-symbolic.svg"
-elif [[ $current == "balanced" ]]; then 
-    echo "$HOME/Dotfiles/nwg-panel/icons/power-profile-balanced-symbolic.svg"
-elif [[ $current == "power-saver" ]]; then
-    echo "$HOME/Dotfiles/nwg-panel/icons/power-profile-power-saver-symbolic.svg"
-fi
