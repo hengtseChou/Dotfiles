20f5fc48d1800428d09f0dcc533dd4fb8f68b787
hengtseChou <hankthedev@gmail.com>
2024-09-09 20:31:05 +0800

    delete unneeded script

--- hypr/scripts/line.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-chromium \
-    --enable-features=UseOzonePlatform \
-    --ozone-platform=wayland \
-    --enable-wayland-ime \
-    --app=chrome-extension://ophjlpahpchlmihnnnihgmmeilfjmjjc/index.html
