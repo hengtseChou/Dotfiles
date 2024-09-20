39cb6a089baf2ac068bfd17baf1e1c5cd3a7689f
hengtseChou <hankthedev@gmail.com>
2024-09-20 22:40:06 +0800

    rewrite dot installing script

--- install.sh
+++ install.sh
@@ -1,44 +1,71 @@
 #!/bin/bash
-_installSymLink() {
-    symlink="$1"
-    linksource="$2"
-    linktarget="$3"
+_DotSymlink() {
 
-    if [ -L "${symlink}" ]; then
-        # is a symlink
-        rm ${symlink}
-        ln -s ${linksource} ${linktarget}
-        echo ":: Symlink ${linksource} -> ${linktarget} created."
-    elif [ -d ${symlink} ]; then
-        # is a dir
-        rm -rf ${symlink}/
-        ln -s ${linksource} ${linktarget}
-        echo ":: Symlink for directory ${linksource} -> ${linktarget} created."
-    elif [ -f ${symlink} ]; then
-        # is a file
-        rm ${symlink}
-        ln -s ${linksource} ${linktarget}
-        echo ":: Symlink to file ${linksource} -> ${linktarget} created."
-    else
-        ln -s ${linksource} ${linktarget}
-        echo ":: New symlink ${linksource} -> ${linktarget} created."
-    fi
+	source="$1"
+	shift
+	PARSED=$(getopt -o '' --long to-home,to-config -- "$@")
+	if [[ $? -ne 0 ]]; then
+		exit 1
+	fi
+	eval set -- "$PARSED"
+
+	target_dir=""
+	while true; do
+		case "$1" in
+			--to-home)
+			target_dir="$HOME"
+			shift
+			;;
+			--to-config)
+			target_dir="$HOME/.config"
+			shift
+			;;
+			--)
+			shift # End of options
+			break
+			;;
+			*)
+			echo "Invalid option: $1" >&2
+			exit 1
+			;;
+		esac
+	done
+
+	target="$target_dir/$(basename $source)"
+
+	if [ -L "${target}" ]; then
+		# is a symlink
+		rm ${target}
+		ln -s ${source} ${target}
+		echo ":: Symlink ${source} -> ${target} created."
+	elif [ -d ${target} ]; then
+		# is a dir
+		rm -rf ${target}/
+		ln -s ${source} ${target}
+		echo ":: Symlink for directory ${source} -> ${target} created."
+	elif [ -f ${target} ]; then
+		# is a file
+		rm ${target}
+		ln -s ${source} ${target}
+		echo ":: Symlink to file ${source} -> ${target} created."
+	else
+		ln -s ${source} ${target}
+		echo ":: New symlink ${source} -> ${target} created."
+	fi
 }
 
 pkgs=(
     brightnessctl
     cliphist
     eza
-    grim
     hypridle
     hyprland
     hyprlock
+    hyprshot
     hyprpaper
-    nwg-panel
     pamixer
     rofi-wayland
     starship
-    swappy
     swaync
     waybar
     wlogout
@@ -51,25 +78,27 @@ echo ":: Checking required packages..."
 sleep 0.5
 yay -S --needed "${pkgs[@]}"
 
-config=$HOME/.config
 dotfiles=$HOME/Dotfiles
 
-_installSymLink $config/alacritty $dotfiles/alacritty $config
-_installSymLink $config/hypr $dotfiles/hypr $config
-_installSymLink $config/htop $dotfiles/htop $config
-_installSymLink $config/rofi $dotfiles/rofi $config
-_installSymLink $config/swappy $dotfiles/swappy $config
-_installSymLink $config/wlogout $dotfiles/wlogout $config
-_installSymLink $config/starship.toml $dotfiles/starship/starship.toml $config
-_installSymLink $config/gtk-3.0 $dotfiles/gtk/gtk-3.0 $config
-_installSymLink $config/gtk-4.0 $dotfiles/gtk/gtk-4.0 $config
-_installSymLink $config/xsettingsd $dotfiles/gtk/xsettingsd $config
-_installSymLink $HOME/.gtkrc-2.0 $dotfiles/gtk/.gtkrc-2.0 $HOME
-_installSymLink $HOME/.Xresources $dotfiles/gtk/.Xresources $HOME
-_installSymLink $HOME/.zshrc $dotfiles/.zshrc $HOME
-_installSymLink $config/swaync $dotfiles/swaync $config
-_installSymLink $config/waybar $dotfiles/waybar $config
+_DotSymlink $dotfiles/alacritty --to-config
+_DotSymlink $dotfiles/chromium/chromium-flags.conf --to-config
+_DotSymlink $dotfiles/code/code.conf --to-config
+_DotSymlink $dotfiles/gtk/gtk-3.0 --to-config
+_DotSymlink $dotfiles/gtk/gtk-4.0 --to-config
+_DotSymlink $dotfiles/gtk/xsettingsd --to-config
+_DotSymlink $dotfiles/gtk/.gtkrc-2.0 --to-home
+_DotSymlink $dotfiles/gtk/.Xresources --to-home
+_DotSymlink $dotfiles/htop --to-config
+_DotSymlink $dotfiles/hypr --to-config
+_DotSymlink $dotfiles/nano/.nanorc --to-home
+_DotSymlink $dotfiles/npm/.npmrc --to-home
+_DotSymlink $dotfiles/R/.Rprofile --to-home
+_DotSymlink $dotfiles/rofi --to-config
+_DotSymlink $dotfiles/wlogout --to-config
+_DotSymlink $dotfiles/starship/starship.toml --to-config
+_DotSymlink $dotfiles/swaync --to-config
+_DotSymlink $dotfiles/waybar --to-config
+_DotSymlink $dotfiles/wlogout --to-config
+_DotSymlink $dotfiles/zsh/.zshrc --to-home
 
-echo ":: Setting up GTK ..."
-source ./hypr/scripts/reload-gtk.sh >/dev/null
 echo ":: Done."
